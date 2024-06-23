286b4e3d31f004f4ef230402e000a0660e0a07b1
watcharaphol.d <dangpiem@hotmail.com>
2024-06-23 10:49:49 +0700

    Create logic to check input number

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 - ถ้า input เท่ากับ 0 ให้โชว์ข้อความ “Zero”
 - ถ้า input น้อยกว่า 0 ให้โชว์ข้อความ “Negative number”
 - ถ้า input ที่รับเข้ามาไม่ใช่ตัวเลข ให้โชว์ข้อความ “Invalid number”
+
+### Homework Finished 23/06/2567
+- นายวัชรพล แดงเปี่ยม (มายด์)
+- Online-CC#18
--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // write code here
+let input = prompt("Enter the number:");
+
+if (input === "") alert("Invalid, Please enter the input number");
+else if (isNaN(+input)) alert("Invalid, The recived input is not number");
+else if (+input > 0) alert("Positive number");
+else if (+input === 0) alert("Zero");
+else if (+input < 0) alert("Negative number");
+else alert("Invalid number");
+
+// Note: The isNaN() function determines whether a value is NaN
